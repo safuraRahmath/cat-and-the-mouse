c43919e92ca3fa1f86feeb4ed36877dd06525be5
safuraRahmath <79985382+safuraRahmath@users.noreply.github.com>
2021-07-06 15:48:46 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,46 +1,47 @@
-var bg;
-var cat,catImg1,catImg2,catImg3;
-var mouse,mouseImg1,mouseImg2,mouseImg3;
+var canvas,bg;
+var together;
+var tom, tomImg1,tomImg2;
+var jerry, jerryImg1,jerryImg2;
+
 function preload() {
-    //load the images here
     bg = loadImage("images/garden.png");
+    tomImg1= loadAnimation("images/tomOne.png");
+    tomImg2=loadAnimation("images/tomTwo.png","images/tomThree.png");
+    tomImg3= loadAnimation("images/tomFour.png");
+    jerryImg1=loadAnimation("images/jerryOne.png");
+    jerryImg2= loadAnimation("images/jerryTwo.png","images/jerryThree.png");
+    jerryImg3=loadAnimation("images/jerryFour.png");
 
-    catImg1 = loadAnimation("images/cat1.png");
-    catImg2 = loadAnimation("images/cat2.png","images/cat3.png");
-    catImg3 = loadAnimation("images/cat4.png");
-
-    mouseImg1 = loadAnimation("images/mouse1.png");
-    mouseImg2 = loadAnimation("images/mouse2.png","images/mouse3.png");
-    mouseImg3 = loadAnimation("images/mouse4.png");
 }
 
 function setup(){
-    createCanvas(1000,800);
-    //create cat and mouse sprites here
-    cat = createSprite(870,600);
-    cat.addAnimation("catSitting",catImg1);
-    cat.scale = 0.2;
-    
-    mouse = createSprite(200,600);
-    mouse.addAnimation("mouseStanding",mouseImg1);
-    mouse.scale  = 0.15;
+    canvas = createCanvas(1000,800);
+
+    tom = createSprite(870, 600);
+    tom.addAnimation("tomSleeping", tomImg1);
+    tom.scale = 0.2;
+
+    jerry = createSprite(200, 600);
+    jerry.addAnimation("jerryStanding", jerryImg1);
+    jerry.scale = 0.15;
+
 }
 
 function draw() {
 
     background(bg);
-    //Write condition here to evalute if cat and mouse 
-    if(cat.x - mouse.x < (cat.width - mouse.width)/2)
+
+    if(tom.x - jerry.x < (tom.width - jerry.width)/2)
     { 
-        cat.velocityX=0;
-        cat.addAnimation("catLastImage", catImg3);
-        cat.x =300;
-        cat.changeAnimation("catLastImage");
-
-        mouse.addAnimation("mouseLastImage", mouseImg3);
-        mouse.changeAnimation("mouseLastImage");
+        tom.velocityX=0;
+        tom.addAnimation("tomLastImage", tomImg3);
+        tom.x =300;
+        tom.scale=0.2;
+        tom.changeAnimation("tomLastImage");
+        jerry.addAnimation("jerryLastImage", jerryImg3);
+        jerry.scale=0.15;
+        jerry.changeAnimation("jerryLastImage");
     }  
-
 
     drawSprites();
 }
@@ -48,17 +49,13 @@ function draw() {
 
 function keyPressed(){
 
-  //For moving and changing animation write code here
-
-
     if(keyCode === LEFT_ARROW){
-        cat.velocityX = -5; 
-        cat.addAnimation("catRunning", catImg2);
-        cat.changeAnimation("catRunning");
+        tom.velocityX = -5; 
+        tom.addAnimation("tomRunning", tomImg2);
+        tom.changeAnimation("tomRunning");
         
-        mouse.addAnimation("mouseTeasing", mouseImg2);
-        mouse.changeAnimation("mouseTeasing");
-
-
+        jerry.addAnimation("jerryTeasing", jerryImg2);
+        jerry.frameDelay = 25;
+        jerry.changeAnimation("jerryTeasing");
     }
 }
